7b0067f3f837bcb4a1fd990c8ce02c3368eaf5dc
antony-jr <antonyjr@protonmail.com>
2022-10-11 15:45:12 +0530

    [skip ci] Update install.sh

--- bash_feedback/install-dev.sh
+++ bash_feedback/install-dev.sh
@@ -7,9 +7,10 @@ mkdir -p ~/.spirit/addons
 wget -O ~/.spirit/addons/bash-feedback "https://github.com/antony-jr/spirit-addons/releases/download/development/bash-feedback-linux-amd64"
 chmod +x ~/.spirit/addons/bash-feedback
 
-wget -O ~/.bashfeedbackrc "https://rawcdn.githack.com/antony-jr/spirit-addons/82c4c6b170738fb3451f015f59de47da58bc4e4b/bash_feedback/bashfeedbackrc.sh"
+wget -O ~/.bashfeedbackrc "https://raw.githubusercontent.com/antony-jr/spirit-addons/main/bash_feedback/bashfeedbackrc.sh"
 
 cp ~/.bashrc ~/.bashrc.backup
+sed -i 'source .bashfeedbackrc' ~/.bashrc
 echo "source .bashfeedbackrc" >> ~/.bashrc
 
 set +x
